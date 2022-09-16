565033b56dcb6d9737ebd9e6551db47cc31fa8e4
RedAlex50 <RedAlex50@mail.ru>
2022-09-16 23:41:22 +0300

    layout 1.2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ var text = [
     "Только ты сам можешь повлиять на свое будущее"
 ];
 
+var help_list = [
+    "http://psi.mchs.gov.ru/",
+    "http://pomoschryadom.ru/",
+    "http://www.твоятерритория.онлайн/",
+    "http://msph.ru/",
+    "http://telefon-doveria.ru/",
+    "http://www.ya-roditel.ru/parents/consultation/",
+    "http://pomogaya-drugim.ru/"
+];
+
+var moreclicked = 0;
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -18,11 +30,24 @@ function More(){
     var replace = document.getElementById("motiv");
     replace.parentNode.replaceChild(motivation, replace);
 
+    var motiv_text_prev = replace.innerHTML;
 
     var motiv_text = text[getRandomInt(0, text.length)];
-    //textes[random i];
-    motivation.append(motiv_text);
     console.log("New text: " + motiv_text);
+    while (motiv_text == motiv_text_prev){
+        motiv_text = text[getRandomInt(0, text.length)];
+        console.log("New text: " + motiv_text);
+    }
+    
+    moreclicked++;
+    motivation.append(motiv_text);
+    motiv_text_prev = motiv_text;
+
+
+    if (moreclicked  == 10){
+        motivation.append(". \n Тебе недостаточно 10 раз?!");
+        moreclicked=0;
+    }
 }
 
 function copy(){
@@ -41,4 +66,9 @@ function copy(){
     console.log("Copied to Clipboard: " + motiv.innerHTML);
     document.execCommand("copy");  
     window.getSelection().removeAllRanges(); 
+}
+
+function help(){
+    var helper = help_list[getRandomInt(0, help_list.length)];
+    document.location.href = helper;
 }
